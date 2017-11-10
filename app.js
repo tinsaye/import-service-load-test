4f3c7b11dbb6350d932b9d23d3d57e3f35946635
tinsaye <abye@studserv.uni-leipzig.de>
2017-11-10 18:20:02 +0100

    grouped requests resolve #7

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const fs = require('fs');
 const now = require('moment');
 const json2csv = require('json2csv');
 
+const groups = new Array(config.groups).fill(1);
+const groupSize = config.groupSize;
+const breakTime = config.breakTime;
 
 // Create the log directory if it does not exist
 if (!fs.existsSync(logDir)) {
@@ -23,7 +26,6 @@ if (!fs.existsSync(archive)) {
 
 let dir = (config.dir === "") ? './' + archive + '/' : config.dir;
 
-
 loadTest.readFiles(dir)
 .then((fileName) => {
     return loadTest.fillBuffer(dir, fileName);
@@ -31,24 +33,34 @@ loadTest.readFiles(dir)
     let numberOfFiles = data.length;
     // create an array with data id so that dataId.length = number of requests
     let dataId = []
-    for (let j = config.request; j > 0; j = j - numberOfFiles) {
-    	// for maximum request = repeat 
-    	if (j < numberOfFiles) numberOfFiles = j;
+    for (let j = groupSize; j > 0; j = j - numberOfFiles) {
+        // for maximum request = repeat 
+        if (j < numberOfFiles) numberOfFiles = j;
         for (let k = 0; k < numberOfFiles; k++) {
             dataId.push(k);
         }
     }
-    return Promise.all(dataId.map((id) => {
-        return loadTest.makeRequest(data[id][0], data[id][1]);
-    })).then((results) => {
+    return Promise.all(groups.map((id) => {
+        setTimeout(() => {
+        }, breakTime);
+        return Promise.all(dataId.map((id) => {
+            return loadTest.makeRequest(data[id][0], data[id][1]);
+        }))
+    }))
+}).then((results) => {
         let file = './log/loadTest_' + now().format('YYYY-MM-DD_HHmmss') + '.csv'
-        let csv = json2csv({data: results, fields: ['statusCode', 'fileName', 'requestTime', 'deckId', 'noOfSlides'], quotes:''})
+        let allResults = [];
+        results.forEach((group) => {
+            group.forEach((request) => {
+                allResults.push(request);
+            })
+        });
+        let csv = json2csv({data: allResults, fields: ['statusCode', 'fileName', 'requestTime', 'deckId', 'noOfSlides'], quotes:''})
         fs.writeFile(file, csv, (err) => {
             if (err) console.error(err.toString());
         });
-        console.log(loadTest.summary(results));
+        console.log(loadTest.summary(allResults));
     })
-})
 .catch((reason) => {
     console.log(reason);
 });
